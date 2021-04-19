1de500b346ff6fb666979e043240bbf504b682fb
meanstackchahal <meanstackchahal@gmail.com>
2021-04-19 15:08:05 +0530

    update files

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import {NgDynamicBreadcrumbModule} from 'ng-dynamic-breadcrumb';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AppRoutingModule.components
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgDynamicBreadcrumbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
